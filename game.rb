1555522285932a99a1c3f4336927edc0e1d6b5b9
Gabriel Caires <gabriel_caires@acaciaconsultoria.com.br>
2022-02-11 18:01:37 -0300

    inicio do jogo

--- game.rb
+++ game.rb
@@ -1,10 +1,20 @@
 require_relative './word.rb'
 
-word = Word.new('palavra')
-word.replace_letter_in_the_hidden_word('a')
-word.replace_letter_in_the_hidden_word('p')
-word.replace_letter_in_the_hidden_word('l')
-word.replace_letter_in_the_hidden_word('v')
-word.replace_letter_in_the_hidden_word('r')
+class Game
+  def initialize(word)
+    @word = word
+    @win = false
+    @amount_errors = 5
+  end
 
-word.print_hidden_word
+  def print_begin_game
+    print "Escolha uma letra: "
+    letter = gets
+
+    if @word.correct_letter?(letter)
+      
+    else
+      
+    end
+  end
+end
